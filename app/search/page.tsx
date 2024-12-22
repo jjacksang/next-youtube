09a4e1de109424cbe670478bd6228ef7375edddb
Sangwoo <jswoo1995@gmail.com>
2024-12-22 20:56:48 +0900

    edit: Logo, Search 컴포넌트 CSS수정

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import style from "./page.module.css";
 
 export default function Search() {
     return (
-        <div>
+        <div className={style.video}>
             <Content />
             <Content />
             <Content />
@@ -13,18 +14,21 @@ export default function Search() {
 export function Content() {
     return (
         <div className={style.container}>
-            <div>navigation</div>
             <div className={style.thumbnail__img}>
-                <img
-                    alt=""
-                    src="https://i.ytimg.com/vi/LY3lQ-sOT1E/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&amp;rs=AOn4CLCb1M9B23bf3wldGnnWBN1r04y2nw"
-                />
+                <Link href={`/video/`}>
+                    <img
+                        alt=""
+                        src="https://i.ytimg.com/vi/LY3lQ-sOT1E/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&amp;rs=AOn4CLCb1M9B23bf3wldGnnWBN1r04y2nw"
+                    />
+                </Link>
             </div>
 
             <div className={style.info__content}>
-                <h3>와... 이건 진짜 영화다</h3>
-                <span>우주하마</span>
-                <span>조회수 13만회 / 2일 전</span>
+                <h3 className={style.content__title}>와... 이건 진짜 영화다</h3>
+                <span className={style.content__author}>우주하마</span>
+                <span className={style.content__info}>
+                    조회수 13만회 / 2일 전
+                </span>
             </div>
         </div>
     );
