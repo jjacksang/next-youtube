15fe8a3e6300a1c9cb3f900588eb981fe58a4146
Sangwoo <jswoo1995@gmail.com>
2025-01-13 21:29:14 +0900

    VideoItem컴포넌트 분리, thumbnail__img hover 적용

---
+++ app/components/video-item.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import style from "./video-item.module.css";
+import Image from "next/image";
+import { elapsedTime } from "../utils/elapsedTime";
+import { Video } from "../utils/type";
+
+interface IVideo {
+    video: Video;
+}
+
+export default function VideoItem({ video }: IVideo) {
+    return (
+        <div className={style.container}>
+            <div className={style.thumbnail__img}>
+                <Link href={`/video/${video.id.videoId}`}>
+                    <Image
+                        alt="h"
+                        width={68}
+                        height={68}
+                        src={video.snippet.thumbnails.medium.url}
+                    />
+                </Link>
+            </div>
+
+            <div className={style.info__content}>
+                <h3 className={style.content__title}>{video.snippet.title}</h3>
+                <div className={style.content__text}>
+                    <span className={style.content__author}>
+                        {video.snippet.channelTitle}
+                    </span>
+                    <span className={style.publishTime}>
+                        <div className={style.dot}>•</div>
+                        {elapsedTime(video.snippet.publishTime)}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,18 +1,12 @@
-import Link from "next/link";
 import style from "./page.module.css";
 import { Suspense } from "react";
 import { fetchYoutubeVideos } from "../utils/api";
-import Image from "next/image";
 import { Video } from "../utils/type";
-import { elapsedTime } from "../utils/elapsedTime";
+import VideoItem from "../components/video-item";
 
 type Props = {
     searchParams: Promise<{ q: string }>;
 };
-
-interface IVideo {
-    video: Video;
-}
 
 async function SearchResult({ q }: { q: string }) {
     console.log(q);
@@ -21,7 +15,7 @@ async function SearchResult({ q }: { q: string }) {
     console.log(res);
 
     return res.items.map((video: Video) => (
-        <Content video={video} key={video.id.videoId} />
+        <VideoItem video={video} key={video.id.videoId} />
     ));
 }
 
@@ -39,35 +33,5 @@ export default async function Search({ searchParams }: Props) {
                 <button>더보기</button>
             </div>
         </section>
-    );
-}
-
-export function Content({ video }: IVideo) {
-    return (
-        <div className={style.container}>
-            <div className={style.thumbnail__img}>
-                <Link href={`/video/${video.id.videoId}`}>
-                    <Image
-                        alt="h"
-                        width={68}
-                        height={68}
-                        src={video.snippet.thumbnails.medium.url}
-                    />
-                </Link>
-            </div>
-
-            <div className={style.info__content}>
-                <h3 className={style.content__title}>{video.snippet.title}</h3>
-                <div className={style.content__text}>
-                    <span className={style.content__author}>
-                        {video.snippet.channelTitle}
-                    </span>
-                    <span className={style.publishTime}>
-                        <div className={style.dot}>•</div>
-                        {elapsedTime(video.snippet.publishTime)}
-                    </span>
-                </div>
-            </div>
-        </div>
     );
 }
