cc93faa2f297c1fd7d4f035437272c2ea3efa3d7
Sangwoo <jswoo1995@gmail.com>
2025-03-23 21:44:00 +0900

    edit: 검색어 변경 시 상태값들을 변경

--- app/search/video-list-client.tsx
+++ app/search/video-list-client.tsx
@@ -17,22 +17,26 @@ export const VideoListClient = ({
     initialVideos: Video[];
     nextPageToken: string;
 }) => {
+    const [currentQuery, setCurrentQuery] = useState(initialQuery);
     const [videos, setVideos] = useState<Video[]>(initialVideos);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [pageToken, setPageToken] = useState<string>(nextPageToken);
     console.log(initialQuery);
     console.log(initialVideos);
     console.log(nextPageToken);
+    console.log("video-list", currentQuery);
 
     // search/page.tsx에서 내려주는 데이터가 변경 시 props로 내려주는
     // initialVideos에 값은 변경되지만 videos에 저장된 값이 다름으로
     // 새로운 데이터를 내려받았을 때 videos값을 변경
-    if (videos !== initialVideos) {
-        setVideos(initialVideos);
-    }
-
-    const searchParams = useSearchParams();
-    const currentQuery = searchParams.get("q") || "";
+    useEffect(() => {
+        if (initialQuery !== currentQuery) {
+            setVideos(initialVideos);
+            setCurrentQuery(initialQuery);
+            setPageToken(nextPageToken);
+            console.log("검색어 변경으로 비디오 목록 초기화: ", initialQuery);
+        }
+    }, [initialQuery, initialVideos, nextPageToken, currentQuery]);
 
     // 더보기 버튼
     const handleLoadMore = async () => {
@@ -58,7 +62,7 @@ export const VideoListClient = ({
 
     return (
         <>
-            <div className={style.video}>
+            <div className={style.video} key="video-item-list">
                 {videos.map((item) => (
                     <VideoItem key={item.id.videoId} video={item} />
                 ))}
