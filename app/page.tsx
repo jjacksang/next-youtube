0627b935cfbfc19934e9e5882c66c7834c68ff65
Sangwoo <jswoo1995@gmail.com>
2025-04-19 23:17:03 +0900

    root/page add img alt

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { VideoSwiper } from "./components/swiper/videoSwiper";
 import styles from "./page.module.css";
-import { fetchYoutubeVideos } from "./utils/api";
 import { processVideoData } from "./utils/process-video-data";
 
 async function DeverloperVideo() {
@@ -8,43 +7,73 @@ async function DeverloperVideo() {
         <>
             <div className={styles.youtuber}>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/tEHzde-2oKTr_PITRkIu8rdBs_0dUiOpWcPjZ-Taa9adri1lejaide2vjnDAhfH4jUn6EP4g=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/tEHzde-2oKTr_PITRkIu8rdBs_0dUiOpWcPjZ-Taa9adri1lejaide2vjnDAhfH4jUn6EP4g=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>이정환 | Winterlood</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/0eIqxAMRNI9gnG0HXSBnpFCmV5E5UjsxzZ3HtvG9Q9PD8QxnZRE9QWvuoiDS8HFEgeFd1Hc4=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/0eIqxAMRNI9gnG0HXSBnpFCmV5E5UjsxzZ3HtvG9Q9PD8QxnZRE9QWvuoiDS8HFEgeFd1Hc4=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>Dave Gray</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/ytc/AIdro_lbqqh0NSFeq6P1F-6qRycC37MtiGyMjZFVsZ4J-c6C474=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/ytc/AIdro_lbqqh0NSFeq6P1F-6qRycC37MtiGyMjZFVsZ4J-c6C474=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>생활코딩</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/ytc/AIdro_lGam0UvO8iBSBx0Aackj6laHETsKi_8uMFfPy9lMmRfA=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/ytc/AIdro_lGam0UvO8iBSBx0Aackj6laHETsKi_8uMFfPy9lMmRfA=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>webdecoded</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>잡캐헨리</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>잡캐헨리</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>잡캐헨리</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>잡캐헨리</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>잡캐헨리</span>
                 </div>
                 <div className={styles.youtuber__profile}>
-                    <img src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj" />
+                    <img
+                        src="https://yt3.googleusercontent.com/1-JruRYG7BEWlF0kjrMaJzGMBC9ywqX0dWwDHPHZRDoZ_UwQdiWLN-1L6BKi2IQsuvl4vEYP1g=s160-c-k-c0x00ffffff-no-rj"
+                        alt="test-img"
+                    />
                     <span>잡캐헨리</span>
                 </div>
             </div>
@@ -52,12 +81,10 @@ async function DeverloperVideo() {
     );
 }
 
-let developerChannelId: IDeveloperId[] = [
+const developerChannelId: IDeveloperId[] = [
     { name: "이정환", key: "UCn7yFtl60fQsRtEaoyuzFUg" },
     { name: "생활코딩", key: "UCvc8kv-i5fvFTJBFAk6n1SA" },
 ];
-
-let arr = {};
 
 interface IDeveloperId {
     name: string;
