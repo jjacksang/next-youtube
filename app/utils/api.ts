28812fbf45fc0b8f2035d8f73fd1c309e8ad70be
jacksang <jswoo1995@gmail.com>
2025-06-01 17:40:09 +0900

    api extends params

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,102 +1,112 @@
-import { RequestInit } from "next/dist/server/web/spec-extension/request";
+import { RequestInit } from 'next/dist/server/web/spec-extension/request';
 
 const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY as string;
 const apiUrl = process.env.NEXT_PUBLIC_YOUTUBE_API_URL as string;
 
 interface IfetchProps {
-    q: string;
-    maxResults: number;
-    nextPageToken?: string;
+  q: string | string[];
+  maxResults: number;
+  nextPageToken?: string;
 }
 
 interface IFetchOptions extends RequestInit {
-    cache?: RequestCache;
-    next?: {
-        revalidate?: number;
-        tags?: string[];
-    };
+  cache?: RequestCache;
+  next?: {
+    revalidate?: number;
+    tags?: string[];
+  };
 }
 
 export const fetchYoutubeVideos = async (
-    { q, maxResults, nextPageToken }: IfetchProps,
-    options?: IFetchOptions
+  { q, maxResults, nextPageToken }: IfetchProps,
+  options?: IFetchOptions,
 ) => {
-    let baseUrl = `${apiUrl}/search?part=snippet&order=date&maxResults=${maxResults}&q=${q}&key=${apiKey}`;
+  const idParam = Array.isArray(q) ? q.join(',') : q;
 
-    if (nextPageToken) {
-        baseUrl += `&pageToken=${nextPageToken}`;
-    }
+  let baseUrl = `${apiUrl}/search?part=snippet&order=date&maxResults=${maxResults}&q=${idParam}&key=${apiKey}`;
 
-    try {
-        const response = await fetch(baseUrl, options);
+  if (nextPageToken) {
+    baseUrl += `&pageToken=${nextPageToken}`;
+  }
 
-        if (response.ok) return response.json();
-    } catch (error) {
-        console.log("Fetch videos Error", error);
-    }
+  try {
+    const response = await fetch(baseUrl, options);
+
+    if (response.ok) return response.json();
+  } catch (error) {
+    console.log('Fetch videos Error', error);
+  }
 };
 
-export const fetchVideoDetails = async ({ id }: { id: string }) => {
-    const response = await fetch(
-        `${apiUrl}/videos?part=snippet%2Cstatistics&id=${id}&key=${apiKey}`
-    );
+export const fetchVideoDetails = async ({ id }: { id: string | string[] }) => {
+  const idParam = Array.isArray(id) ? id.join(',') : id;
 
-    if (!response.ok) {
-        throw new Error(`Failed fetch video details: ${response.status}`);
-    } else {
-        return response.json();
-    }
+  const response = await fetch(
+    `${apiUrl}/videos?part=snippet%2Cstatistics&id=${idParam}&key=${apiKey}`,
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed fetch video details: ${response.status}`);
+  } else {
+    return response.json();
+  }
 };
 
 export const fetchChannelDetails = async ({
-    id,
+  id,
 }: {
-    id: string | string[];
+  id: string | string[];
 }) => {
-    const idParam = Array.isArray(id) ? id.join(",") : id;
-    console.log(idParam);
+  const idParam = Array.isArray(id) ? id.join(',') : id;
+  console.log(idParam);
 
-    const response = await fetch(
-        `${apiUrl}/channels?part=snippet%2CtopicDetails%2Cstatistics&id=${idParam}&key=${apiKey}`
-    );
-    if (!response.ok) {
-        throw new Error(`failed fetch channel details: ${response.status}`);
-    } else {
-        return response.json();
-    }
+  const response = await fetch(
+    `${apiUrl}/channels?part=snippet%2CtopicDetails%2Cstatistics&id=${idParam}&key=${apiKey}`,
+  );
+  if (!response.ok) {
+    throw new Error(`failed fetch channel details: ${response.status}`);
+  } else {
+    return response.json();
+  }
 };
 
-export const fetchPlaylistDetails = async ({ id }: { id: string }) => {
-    const response = await fetch(
-        `${apiUrl}/playlistItems?part=snippet&playlistId=${id}&key=${apiKey}`
-    );
-    if (!response.ok) {
-        throw new Error(`failed fetch playlist details: ${response.status}`);
-    } else {
-        return response.json();
-    }
+export const fetchPlaylistDetails = async ({
+  id,
+}: {
+  id: string | string[];
+}) => {
+  const idParam = Array.isArray(id) ? id.join(',') : id;
+
+  const response = await fetch(
+    `${apiUrl}/playlistItems?part=snippet&playlistId=${idParam}&key=${apiKey}`,
+  );
+  if (!response.ok) {
+    throw new Error(`failed fetch playlist details: ${response.status}`);
+  } else {
+    return response.json();
+  }
 };
 
 export const fetchCommentList = async ({
-    id,
-    pageToken,
+  id,
+  pageToken,
 }: {
-    id: string;
-    pageToken: string;
+  id: string;
+  pageToken: string;
 }) => {
-    let baseUrl = `${apiUrl}/commentThreads?part=snippet&maxResults=12&videoId=${id}&key=${apiKey}`;
+  let baseUrl = `${apiUrl}/commentThreads?part=snippet&maxResults=12&videoId=${id}&key=${apiKey}`;
 
-    if (pageToken) {
-        baseUrl += `&pageToken=${pageToken}`;
-    }
+  if (pageToken) {
+    baseUrl += `&pageToken=${pageToken}`;
+  }
 
-    console.log(baseUrl);
-    const response = await fetch(baseUrl);
+  console.log(baseUrl);
+  const response = await fetch(baseUrl);
 
-    if (!response.ok) {
-        console.error(`comment list Error: ${response.statusText}`);
-        throw new Error(`failed fetch comment list: ${response.status}`);
-    } else {
-        return response.json();
-    }
+  if (!response.ok) {
+    console.error(`comment list Error: ${response.statusText}`);
+    throw new Error(`failed fetch comment list: ${response.status}`);
+  } else {
+    return response.json();
+  }
 };
