453acc2c7e2f42c9848041c9ba76db8522ae25f3
jacksang <jswoo1995@gmail.com>
2025-06-15 17:18:37 +0900

    fix: channel thumbnail fixed

--- app/components/reco-channel.tsx
+++ app/components/reco-channel.tsx
@@ -1,34 +1,34 @@
-import Image from "next/image";
-import { IChannel } from "../utils/type";
-import style from "./reco-channel.module.css";
+import Image from 'next/image';
+import { IChannel } from '../utils/type';
+import style from './reco-channel.module.css';
 
 interface RecoChannelProps {
-    channel: IChannel;
+  channel: IChannel;
 }
 
 export const RecoChannel = ({ channel }: RecoChannelProps) => {
-    console.log("reco-channel >>> ", channel);
-    return (
-        <div className={style.channel__container}>
-            <div className={style.channel__thumbnail}>
-                <Image
-                    src={channel.snippet.thumbnails.high.url}
-                    alt={channel.snippet.description}
-                    width={180}
-                    height={180}
-                    className={style.channel__img}
-                />
-            </div>
-            <div className={style.channel__info}>
-                <div className={style.channel__text}>
-                    <h2>{channel.snippet.channelTitle}</h2>
-                    <span></span>
-                    <span>{channel.snippet.description}</span>
-                </div>
-                <div className={style.subscribe__button}>
-                    <button>구독</button>
-                </div>
-            </div>
+  console.log('reco-channel >>> ', channel);
+  return (
+    <div className={style.channel__container}>
+      <div className={style.channel__thumbnail}>
+        <Image
+          src={channel.snippet.thumbnails.default.url}
+          alt={channel.snippet.description}
+          width={120}
+          height={120}
+          className={style.channel__img}
+        />
+      </div>
+      <div className={style.channel__info}>
+        <div className={style.channel__text}>
+          <h2>{channel.snippet.channelTitle}</h2>
+          <span></span>
+          <span>{channel.snippet.description}</span>
         </div>
-    );
+        <div className={style.subscribe__button}>
+          <button>구독</button>
+        </div>
+      </div>
+    </div>
+  );
 };
