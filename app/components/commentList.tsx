6b0b679bdac09852c4cd70e60bb16c4b9bb98b52
jacksang <jswoo1995@gmail.com>
2025-06-25 20:50:20 +0900

    comment list img > next_image

--- app/components/commentList.tsx
+++ app/components/commentList.tsx
@@ -1,69 +1,53 @@
-import style from "./commentList.module.css";
+import style from './commentList.module.css';
 
-import { ICommentList } from "../utils/type";
-import { elapsedTime } from "../utils/elapsedTime";
-import Link from "next/link";
+import { ICommentList } from '../utils/type';
+import { elapsedTime } from '../utils/elapsedTime';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export const CommentList = ({ comments }: { comments: ICommentList[] }) => {
-    console.log("CommnetList component!!", comments);
+  console.log('CommnetList component!!', comments);
 
-    return (
-        <>
-            {comments.map((item: ICommentList) => (
-                <div className={style.comment__container} key={item.etag}>
-                    <div>
-                        <Link
-                            href={`/channel/${item.snippet.topLevelComment.snippet.authorChannelId.value}`}
-                            className={style.author__img}
-                        >
-                            <img
-                                src={
-                                    item.snippet.topLevelComment.snippet
-                                        .authorProfileImageUrl
-                                }
-                                alt={
-                                    item.snippet.topLevelComment.snippet
-                                        .authorChannelId.value
-                                }
-                            />
-                        </Link>
-                    </div>
-                    <div className={style.main__container}>
-                        <Link
-                            href={
-                                item.snippet.topLevelComment.snippet
-                                    .authorChannelUrl
-                            }
-                            className={style.author}
-                        >
-                            <span className={style.author__comment}>
-                                {
-                                    item.snippet.topLevelComment.snippet
-                                        .authorDisplayName
-                                }
-                            </span>
-                            <span className={style.publishTime}>
-                                {elapsedTime(
-                                    item.snippet.topLevelComment.snippet
-                                        .publishedAt
-                                )}
-                            </span>
-                        </Link>
+  return (
+    <>
+      {comments.map((item: ICommentList) => (
+        <div className={style.comment__container} key={item.etag}>
+          <div>
+            <Link
+              href={`/channel/${item.snippet.topLevelComment.snippet.authorChannelId.value}`}
+              className={style.author__img}
+            >
+              <Image
+                src={item.snippet.topLevelComment.snippet.authorProfileImageUrl}
+                alt={item.snippet.topLevelComment.snippet.authorChannelId.value}
+                width={36}
+                height={36}
+                className={style.user__comment__img}
+              />
+            </Link>
+          </div>
+          <div className={style.main__container}>
+            <Link
+              href={item.snippet.topLevelComment.snippet.authorChannelUrl}
+              className={style.author}
+            >
+              <span className={style.author__comment}>
+                {item.snippet.topLevelComment.snippet.authorDisplayName}
+              </span>
+              <span className={style.publishTime}>
+                {elapsedTime(item.snippet.topLevelComment.snippet.publishedAt)}
+              </span>
+            </Link>
 
-                        <div className={style.comment}>
-                            <span>
-                                {
-                                    item.snippet.topLevelComment.snippet
-                                        .textDisplay
-                                }
-                            </span>
-                        </div>
-                        <div className={style.comment__icon}>
-                            {item.snippet.topLevelComment.snippet.likeCount}
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </>
-    );
+            <div className={style.comment}>
+              <span>{item.snippet.topLevelComment.snippet.textDisplay}</span>
+            </div>
+            <div className={style.comment__icon}>
+              {item.snippet.topLevelComment.snippet.likeCount}
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
