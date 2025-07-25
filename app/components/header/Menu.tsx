cab66cde37139ec7d0608e3362b81c4f2ba35c0d
jacksang <jswoo1995@gmail.com>
2025-07-25 23:04:09 +0900

    remove: Menu searchWord remove

--- app/components/header/Menu.tsx
+++ app/components/header/Menu.tsx
@@ -1,38 +1,25 @@
-import style from "./Menu.module.css";
+import style from './Menu.module.css';
 
-import { headerMenus, searchKeyword } from "@/app/data/header";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { headerMenus } from '@/app/data/header';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Menu = () => {
-    const location = usePathname();
-    return (
-        <nav className={style.menu}>
-            <ul className={style.menuList}>
-                {headerMenus.map((menu, key) => (
-                    <li
-                        key={key}
-                        className={location === menu.src ? "active" : ""}
-                    >
-                        <Link href={menu.src}>
-                            {menu.icon}
-                            <span>{menu.title}</span>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <ul className={style.keyword}>
-                {searchKeyword.map((keyword, key) => (
-                    <li
-                        key={key}
-                        className={location === keyword.src ? "active" : ""}
-                    >
-                        <Link href={keyword.src}>{keyword.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+  const location = usePathname();
+  return (
+    <nav className={style.menu}>
+      <ul className={style.menuList}>
+        {headerMenus.map((menu, key) => (
+          <li key={key} className={location === menu.src ? 'active' : ''}>
+            <Link href={menu.src}>
+              {menu.icon}
+              <span>{menu.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
