1d4c963ea6f7193dc9ea023c6f77e0a68a58e2ed
jacksang <jswoo1995@gmail.com>
2025-08-03 16:57:42 +0900

    feat:short page

--- app/shorts/page.tsx
+++ app/shorts/page.tsx
@@ -1,7 +1,19 @@
+import styles from './page.module.css';
+
 export default function Shorts() {
-    return (
-        <div>
-            <h1>Shorts Page</h1>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles.short__container}>
+        <section className={styles.short__section}>Short section</section>
+      </div>
+      <div className={styles.toggle}>
+        <form className={styles.toggle__form}>
+          <button type="button">좋아요</button>
+          <button type="button">싫어요</button>
+          <button type="button">댓글</button>
+          <button type="button">공유</button>
+        </form>
+      </div>
+    </div>
+  );
 }
