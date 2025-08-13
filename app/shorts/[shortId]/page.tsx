212d582d3c4feb82618c5863372e700756e771a1
jacksang <jswoo1995@gmail.com>
2025-08-13 21:46:46 +0900

    fix: short api url edit contentDetails > statistisc

--- app/shorts/[shortId]/page.tsx
+++ app/shorts/[shortId]/page.tsx
@@ -12,7 +12,7 @@ async function getShortVideos(shortId: string) {
   console.log(shortId);
   try {
     const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&chart=mostPopular&maxResults=24&regionCode=KR&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&chart=mostPopular&maxResults=24&regionCode=KR&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
       {
         next: { revalidate: 300 },
       },
