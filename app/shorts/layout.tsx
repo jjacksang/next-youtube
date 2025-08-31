07548edfc84806bb5bab88c48458388cf3c30f7b
jacksang <jswoo1995@gmail.com>
2025-08-31 14:44:39 +0900

    edit: shorts/layout PlayerProvider init

--- app/shorts/layout.tsx
+++ app/shorts/layout.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
+import { PlayerProvider } from '../contexts/player-context';
 
 export default function Layout({
   children,
@@ -9,6 +10,8 @@ export default function Layout({
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <PlayerProvider>{children}</PlayerProvider>
+    </QueryClientProvider>
   );
 }
