c3f049b23ca503d9da3ee0696128f02298f8960f
jacksang <jswoo1995@gmail.com>
2025-09-11 21:57:35 +0900

    feat: root metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,33 @@
-import Header from "./components/header/header";
-import Searchbar from "./components/searchbar";
-import style from "./layout.module.css";
-import "./globals.css";
+import Header from './components/header/header';
+import Searchbar from './components/searchbar';
+import style from './layout.module.css';
+import './globals.css';
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Suspense } from "react";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="ko">
-            <body>
-                <Header />
-                <main className={style.main} id="main" role="main">
-                    <Searchbar />
-                    <Suspense>{children}</Suspense>
-                </main>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="ko">
+      <body>
+        <Header />
+        <main className={style.main} id="main" role="main">
+          <Searchbar />
+          <Suspense>{children}</Suspense>
+        </main>
+      </body>
+    </html>
+  );
 }
+
+export const metadata: Metadata = {
+  title: 'Choptube Project',
+  description: 'Choptube에 오신걸 환영합니다.',
+};
