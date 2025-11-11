444d98c2f3709f96ee87fab016b41c32707822f2
jacksang <jswoo1995@gmail.com>
2025-11-11 20:59:55 +0900

    delete: useSearchInfinityQuery exchange

--- app/hooks/useSearchInfiniteQuery.ts
+++ app/hooks/useSearchInfiniteQuery.ts
@@ -1,25 +1,22 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { fetchYoutubeVideos } from '../utils/api';
-import { processVideoData } from '../utils/process-video-data';
-import { IChannel, IEnrichedPlaylist, IEnrichedVideo } from '../utils/type';
-import { useMemo, useRef } from 'react';
+import { Video } from '../components/video/video-item2';
+import { fetchSearchVideos } from '../services/fetchSearch';
+import { parseJson } from '../utils/fetcher';
+import { fetchVideoDetails } from '../services/fetchVideoDetail';
+import { fetchChannelDetails } from '../services/fetchChannelDetails';
 
-type YoutubeItems = (IEnrichedVideo | IEnrichedPlaylist | IChannel)[];
-
-export interface InitialYoutubeData {
-  items: YoutubeItems;
-  nextPageToken?: string;
+interface InitialYoutubeVideos {
+  items: Video[];
+  nextPageToken: string;
 }
 
 interface ProcessedYoutubePageData {
-  videos: (IEnrichedVideo | IEnrichedPlaylist)[];
-  channels: IChannel[];
+  videos: Video[];
   nextPageToken?: string;
 }
 
 interface UseSearchInfiniteQueryReturn {
-  allVideos: (IEnrichedVideo | IEnrichedPlaylist)[];
-  allChannels: IChannel[];
+  videos: Video[];
 
   status: 'error' | 'pending' | 'success';
   isFetching: boolean;
@@ -33,37 +30,10 @@ interface UseSearchInfiniteQueryReturn {
   totalPages: number;
 }
 
-export default function useSearchInfiniteQuery(
+export default function useSearchInfiniteQuery2(
   searchParams: string,
-  initialData: InitialYoutubeData,
+  initialVideos: InitialYoutubeVideos,
 ): UseSearchInfiniteQueryReturn {
-  const existingVideoIds = useRef<Set<string>>(new Set());
-
-  const initialFilteredVideos: (IEnrichedVideo | IEnrichedPlaylist)[] = [];
-  const initialFilteredChannels: IChannel[] = [];
-
-  if (initialData?.items) {
-    for (const item of initialData.items) {
-      if (item.id.kind === 'youtube#channel') {
-        initialFilteredChannels.push(item as IChannel);
-      } else {
-        const id =
-          item.id.kind === 'youtube#video'
-            ? item.id.videoId
-            : item.id.kind === 'youtube#playlist'
-              ? item.id.playlistId
-              : undefined;
-
-        if (id && !existingVideoIds.current.has(id)) {
-          existingVideoIds.current.add(id);
-          initialFilteredVideos.push(
-            item as IEnrichedVideo | IEnrichedPlaylist,
-          );
-        }
-      }
-    }
-  }
-
   const {
     status,
     data,
@@ -76,139 +46,78 @@ export default function useSearchInfiniteQuery(
     queryKey: ['videos', searchParams],
     queryFn: async ({ pageParam }) => {
       console.log(pageParam);
-      const targetCount = 24;
-      const collectedVideos: (IEnrichedVideo | IEnrichedPlaylist)[] = [];
-      const collectedChannels: IChannel[] = [];
       let nextPageToken = pageParam as string | undefined;
 
-      const MAX_ATTEMPTS = 5;
-      let attemptCount = 0;
+      const response = await fetchSearchVideos({
+        q: searchParams,
+        maxResults: 24,
+        order: 'date',
+        nextPageToken,
+      });
 
-      while (
-        collectedVideos.length < targetCount &&
-        attemptCount < MAX_ATTEMPTS
-      ) {
-        const response = await fetchYoutubeVideos({
-          q: searchParams,
-          maxResults: targetCount - collectedVideos.length,
-          nextPageToken,
-        });
+      const results = await parseJson(response);
+      console.log(results);
 
-        if (!response.ok) {
-          throw new Error(
-            `Failed to fetch: ${response.status} ${response.statusText}`,
-          );
-        }
+      // video id목록 추출 후 detail 요청
+      const ids = results.items.map(({ id }) => id.videoId);
+      const detailResponse = await fetchVideoDetails({ ids });
+      const parsedDetail = await parseJson(detailResponse);
 
-        const searchResults = await response.json();
-        const { videoWithViewCount, nextPageToken: newToken } =
-          await processVideoData(searchResults);
+      // channel id목록 추출 후 thumbnails 요청
+      const channelIds = parsedDetail.items.map(id => id.snippet.channelId);
+      const channelDetailsResponse = await fetchChannelDetails({ channelIds });
+      const parsedChannelDetails = await parseJson(channelDetailsResponse);
 
-        const newVideos: (IEnrichedVideo | IEnrichedPlaylist)[] = [];
-        const newChannels: IChannel[] = [];
-
-        nextPageToken = newToken;
-        attemptCount++;
-
-        for (const item of videoWithViewCount) {
-          if (item.id.kind === 'youtube#channel') {
-            newChannels.push(item as IChannel);
-            continue;
-          }
-
-          const id =
-            item.id.kind === 'youtube#video'
-              ? item.id.videoId
-              : item.id.kind === 'youtube#playlist'
-                ? item.id.playlistId
-                : undefined;
-          if (id && !existingVideoIds.current.has(id)) {
-            newVideos.push(item as IEnrichedVideo | IEnrichedPlaylist);
-            existingVideoIds.current.add(id);
-          }
-        }
-
-        collectedVideos.push(...newVideos);
-        collectedChannels.push(...newChannels);
-      }
+      const channelThumbnailMap = parsedChannelDetails.items.reduce(
+        (acc, { id, snippet }) => {
+          acc[id] = snippet.thumbnails.default.url ?? null;
+          return acc;
+        },
+        {} as Record<string, string | null>,
+      );
 
       return {
-        videos: collectedVideos.slice(0, targetCount),
-        channels: collectedChannels,
-        nextPageToken,
+        videos: parsedDetail.items.map(({ id, snippet, statistics }) => ({
+          id: id,
+          channelId: snippet.channelId,
+          description: snippet.description,
+          thumbnailUrl: snippet.thumbnails.medium.url,
+          channelThumbnailUrl: channelThumbnailMap[snippet.channelId] ?? null,
+          title: snippet.title,
+          channelTitle: snippet.channelTitle,
+          viewCount: statistics.viewCount,
+          publishTime: snippet.publishedAt,
+        })),
+        nextPageToken: results.nextPageToken,
       };
     },
-
-    getNextPageParam: lastPage => {
-      return lastPage.nextPageToken || undefined;
-    },
-
-    initialPageParam: undefined,
-    initialData: initialData
+    getNextPageParam: lastPage => lastPage.nextPageToken || undefined,
+    initialPageParam: initialVideos.nextPageToken,
+    initialData: initialVideos
       ? {
           pages: [
             {
-              channels: initialData.items.filter(
-                (item): item is IChannel => item.id.kind === 'youtube#channel',
-              ),
-              videos: initialData.items.filter(
-                (item): item is IEnrichedVideo | IEnrichedPlaylist =>
-                  item.id.kind === 'youtube#video' ||
-                  item.id.kind === 'youtube#playlist',
-              ),
-              nextPageToken: initialData.nextPageToken,
+              videos: initialVideos.items,
+              nextPageToken: initialVideos.nextPageToken,
             },
           ],
           pageParams: [undefined],
         }
       : undefined,
     staleTime: 5 * 60 * 1000, // 5분
-    gcTime: 30 * 60 * 1000, // 30분
-
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
+    gcTime: 30 * 60 * 1000,
   });
 
-  const allVideos = useMemo((): (IEnrichedVideo | IEnrichedPlaylist)[] => {
-    if (!data?.pages) {
-      console.log('allVideos is empty');
-      return [];
-    }
-    return data.pages.flatMap(page => page.videos);
-  }, [data?.pages]);
-
-  const allChannels = useMemo((): IChannel[] => {
-    if (!data?.pages) {
-      console.log('allChannel is empty');
-      return [];
-    }
-
-    const channels = data.pages.flatMap(page => page.channels);
-    console.log('all channels count :', channels.length);
-    return channels;
-  }, [data?.pages]);
-
-  const currentPageToken = useMemo(() => {
-    if (!data?.pages || data.pages.length === 0) return undefined;
-    return data.pages[data.pages.length - 1].nextPageToken;
-  }, [data?.pages]);
+  const videos = data?.pages?.flatMap(page => page.videos);
 
   return {
-    // 데이터
-    allChannels,
-    allVideos,
-
-    // 상태
+    videos,
     status,
+    error,
     isFetching,
+    fetchNextPage,
     isFetchingNextPage,
     hasNextPage,
-    error: error as Error | null,
-
-    // 액션함수
-    fetchNextPage,
-
-    currentPageToken,
     totalPages: data?.pages?.length || 0,
   };
 }
